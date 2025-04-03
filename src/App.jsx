aee6b7b1bd5bd2085696c1218829d1852e36960e
Shaurya Rahlon <shauryarahlon.11@gmail.com>
2025-04-03 18:55:53 +0530

    Fixing Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home';
+import Home from './Pages/Home/Home';
+
 
 function App() {
   return (
